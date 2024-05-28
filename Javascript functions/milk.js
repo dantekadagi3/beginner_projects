af0251f53007703b6e34abd1c0c5b3dc224b4494
dantekadagi3 <135029772+dantekadagi3@users.noreply.github.com>
2024-05-28 12:13:24 -0700

    updates

---
+++ Javascript functions/milk.js
@@ -0,0 +1,12 @@
+function getMilk(bottles){
+    var cost=bottles*1.5;
+    return cost;
+}
+
+var result=getMilk(8);
+console.log(result);
+
+
+var n=Math.random()*6 +1;
+n=Math.floor(n);
+console.log(n);
